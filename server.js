122fa8a7aba346131aab02fc1effac1ad8723d28
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-10-28 19:56:08 -0400

    reformat

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
-'use strict';
+"use strict";
 
-// ================================================================
-// get all the tools we need
-// ================================================================
-var express = require('express');
-var routes = require('./routes/index.js');
+var express = require("express");
+var routes = require("./routes/index.js");
 var port = process.env.PORT || 3000;
 
 var app = express();
 
-// ================================================================
-// setup our express application
-// ================================================================
-app.use('/public', express.static(process.cwd() + '/public'));
-app.set('view engine', 'ejs');
+app.use("/public", express.static(process.cwd() + "/public"));
+app.set("view engine", "ejs");
 
-
-// ================================================================
-// setup routes
-// ================================================================
 routes(app);
 
-// ================================================================
-// start our server
-// ================================================================
 app.listen(port, function() {
-    console.log('Server listening on port ' + port + '...');
+  console.log("Server listening on port " + port + "...");
 });
